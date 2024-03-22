7d05619d418e290d7f5c8f40217b48a9f75b64df
Ebukings5 <cristofarokoye@gmail.com>
2024-03-22 21:12:13 +0100

    configuration

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-# Create a file in /tmp, using puppet
+# created a file in /tmp using Puppet
 file { '/tmp/school':
-content => 'I love Puppet',
-mode 	=> '0744',
-owner 	=> 'www-data',
-group 	=> 'www-data',
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
