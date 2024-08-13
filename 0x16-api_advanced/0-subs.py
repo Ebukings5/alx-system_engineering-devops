113b2ceb5c7e3678c5bcc929e84240655d53b9f6
Ebukings5 <cristofarokoye@gmail.com>
2024-08-13 17:01:47 +0100

    Api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,24 +3,24 @@
 import requests
 
 def number_of_subscribers(subreddit):
-	"""
-		Returns number of subscribers for a given subreddit
-	"""
-	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	headers = {"User-Agent": "Mozzila/5.0"}
-	response = requests.get(url, headers=headers)
-	if response.status_code == 200:
-		data = response.json()
-		return data['data']['subscribers']
-	else:
-		return 0
+        """
+                Returns number of subscribers for a given subreddit
+        """
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        headers = {"User-Agent": "Mozzila/5.0"}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+                data = response.json()
+                return data['data']['subscribers']
+        else:
+                return 0
 
 
-	if __name__ == '__main__':
+        if __name__ == '__main__':
 
-	import sys
+        import sys
 
-	if len(sys.argv) < 2:
-		print("Please pass an argument for the subreddit to search.")
-	else:
-		print(number_of_subscribers(sys.argv[1]))
+        if len(sys.argv) < 2:
+                print("Please pass an argument for the subreddit to search.")
+        else:
+                print(number_of_subscribers(sys.argv[1]))
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,24 +7,23 @@ import requests
 
 def top_ten(subreddit):
     """
-    Prints the titles of the first 10 hot posts listed for a given subreddit
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
     """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
         data = response.json()
         posts = data['data']['children']
-        if posts:
-            for post in posts:
-                print(post['data']['title'])
-        else:
-            print("No posts found for the subreddit:", subreddit)
+        for post in posts:
+            print(post['data']['title'])
     else:
         print("None")
 
-import sys
-    if __name__ == "__main__":
+
+if __name__ == "__main__":
+    import sys
 
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
