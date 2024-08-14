bb43b22086c5e3350528f9322f9472a2b9d7db6a
Ebukings5 <cristofarokoye@gmail.com>
2024-08-14 18:47:30 +0100

    Webstack debug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 # Puppet manifest to fix Apache returning a 500 error
 
 exec { 'fix wordpress':
-  command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path        => '/usr/local/bin:/bin',
+	command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+	path        => '/usr/local/bin:/bin',
 }
